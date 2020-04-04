c032b53143cf8c67958ca8936ac0b64bb8fd29dc
Tyler Karol <tyler.karol.17@cnu.edu>
2020-04-04 00:35:46 -0700

    almost done

--- FileIO.cpp
+++ FileIO.cpp
@@ -13,12 +13,100 @@ using namespace std;
 
 int KP_FileIO::getFileContents(const std::string &filename, std::string &contents)
 {
-	//TODO fill in
+	//local variables
+	ifstream inputFile;
+	vector<string> myEntryVector;
+	string line;
+	string temp = "";
+
+	//make sure 'contents' is empty
+	contents.clear();
+
+	//open up 'filename' to read
+	inputFile.open( filename, ios::in );
+	if( !inputFile.is_open() )
+		return COULD_NOT_OPEN_FILE_TO_READ;
+
+	//need something to store a new liine from the file
+
+	//loop through the file until the end
+	while( !inputFile.eof() ) {
+		getline( inputFile, line );
+					if( line.empty() )
+						break;
+
+					//append this new line to 'contents'
+
+				myEntryVector.push_back(line);
+				contents.append(line);
+	}
+
+
+	//once finished, close the file if its open
+	inputFile.close();
+
+	//exit function
+	return SUCCESS;
 }
 
 int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
 {
+	// Local variables
+	ofstream outputFile;
+
 	//TODO fill in
+	//open up 'filename' to write
+	outputFile.open( filename, std::ofstream::out | std::ofstream::trunc );
+	if( !outputFile.is_open() )
+		return COULD_NOT_OPEN_FILE_TO_WRITE;
+
+	//loop through the vector via iterators
+		//push each entry to the file
+
+	for( string entrys : myEntryVector ) {
+			outputFile
+				<< entrys;
+		}
+
+	//once finished, close the file if its open
+	//exit the function
+	outputFile.close();
+	return SUCCESS;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
